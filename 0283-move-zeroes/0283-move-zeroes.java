e7b9e93a38401ffce31159b338a9f2b4bd3d3822
Aryan Soni <ashoksoni9425@gmail.com>
2026-07-30 17:40:36 +0530

    Time: 1 ms (99.88%), Space: 48.1 MB (6.81%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -0,0 +1,13 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int insert=0;
+        for(int num:nums){
+            if(num!=0){
+                nums[insert++]=num;
+            }
+        }
+        while(insert<nums.length){
+            nums[insert++]=0;
+        }
+    }
+}
